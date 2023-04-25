f747185dc2394f1f9274a9e63b377eba7126d318
João Guilherme Pretti Genari <90649576+joaocaju1@users.noreply.github.com>
2023-04-25 20:48:33 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
     arrayDeTarefas: Tarefa[] = [];
     apiURL : string;
     constructor(private http: HttpClient) {
-        this.apiURL = 'https://todoapp-api-ten.vercel.app'
+        this.apiURL = 'https://fron-tapp-git-main-joaocaju1.vercel.app/'
 ;
         this.READ_tarefas();
     }
